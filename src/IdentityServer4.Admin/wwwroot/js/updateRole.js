b097ad62c96991efe0efba3f6603b7c80a0f63ef
Lewis Zou <zlzforever@163.com>
2019-01-23 17:47:31 +0800

    fix validate issue

--- src/IdentityServer4.Admin/wwwroot/js/updateRole.js
+++ src/IdentityServer4.Admin/wwwroot/js/updateRole.js
@@ -30,12 +30,12 @@ $(function () {
                     this.errors.push("角色名不能为空");
                 }
                 else {
-                    if (!app.rangeCheck(this.el.name, 4, 30)) {
+                    if (!app.rangeCheck(this.el.name.length, 4, 30)) {
                         this.errors.push("角色名长度范围为 4-30");
                     }
                 }
                 if (app.requireCheck(this.el.description)) {
-                    if (!app.maxCheck(this.el.description, 1000)) {
+                    if (!app.maxCheck(this.el.description.length, 1000)) {
                         this.errors.push("角色描述不能超过 1000 字符");
                     }
                 }
@@ -66,8 +66,8 @@ $(function () {
     function loadView(vue) {
         const url = '/api/role/' + app.getPathPart(window.location.href, 1);
         app.get(url, function (result) {
-            vue.$data.name = result.data.name;
-            vue.$data.description = result.data.description;
+            vue.$data.el.name = result.data.name;
+            vue.$data.el.description = result.data.description;
         });
     }
 });
